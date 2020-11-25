2d394472ede81a72004f0495ce0835d1471ef091
dusjagr <marc@dusseiller.ch>
2020-11-25 01:19:51 +0100

    fixing the island

---
+++ hackteria/floors.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floors" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <editorsettings>
+  <export target="." format=""/>
+ </editorsettings>
+ <image source="floors.png" width="256" height="256"/>
+ <tile id="37">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
