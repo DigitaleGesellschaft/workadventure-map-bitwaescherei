87723b3f17ae857646cf831fa8b8c06f0a9b68f7
dusjagr <marc@dusseiller.ch>
2020-12-22 11:00:22 +0100

    replace tileset

---
+++ tiles/d4hnezn_fixed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="d4hnezn_fixed" tilewidth="32" tileheight="32" tilecount="2744" columns="8">
+ <image source="../d4hnezn_fixed.png" width="256" height="11000"/>
+</tileset>
