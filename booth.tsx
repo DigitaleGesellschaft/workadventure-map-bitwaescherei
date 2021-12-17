453a512ca0f9ac3cfe7f0d21571fb6d039f70108
Tillo <tillo.bosshart@gmail.com>
2021-12-17 10:55:47 +0100

    photobox added

---
+++ booth.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="booth" tilewidth="32" tileheight="32" tilecount="10" columns="10">
+ <image source="../../wag/booth.png" width="320" height="32"/>
+</tileset>
