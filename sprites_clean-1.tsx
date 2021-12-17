78dd4d57850d1950a38c3ec5b675442c48fbb873
Tillo <tillo.bosshart@gmail.com>
2021-12-17 09:42:53 +0100

    initial

---
+++ sprites_clean-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="sprites_clean-1" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="sprites_clean-1.png" width="1024" height="512"/>
+</tileset>
