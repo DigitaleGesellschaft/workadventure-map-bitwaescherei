60f580070405f63463784dadef3360b9c559dc15
Tillo <tillo.bosshart@gmail.com>
2021-12-25 15:48:33 +0100

    moved files

--- tiles/bosque_expansion_cropped_top.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="bosque_expansion_cropped_top" tilewidth="32" tileheight="32" tilecount="1024" columns="8">
- <image source="../bosque_expansion_cropped_top.png" width="256" height="4096"/>
-</tileset>
